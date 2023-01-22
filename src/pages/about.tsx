6cffbe82380ee6261e9967a4fb3fa88ebfaa6a23
Omar Ouhra <omarmaroc26@gmail.com>
2023-01-22 21:44:48 +0100

    fix: pages content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function about() {
   return (
-    <div>about</div>
-  )
+    <div className="flex flex-col max-w-4xl mx-auto  items-center justify-center mt-24">
+      <h1 className="text-5xl  font-semibold">About Page</h1>
+    </div>
+  );
 }
 
-export default about
+export default about;
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function work() {
-  return <div>work</div>;
+  return (
+    <div className="flex flex-col max-w-4xl mx-auto  items-center justify-center mt-24">
+      <h1 className="text-5xl  font-semibold">Work Page</h1>
+    </div>
+  );
 }
 
 export default work;
